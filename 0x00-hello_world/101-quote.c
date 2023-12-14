b6c87f43b3dbad858bc60967cf0ac8a57f8c0353
rallmac <tobijahekperipke@gmail.com>
2023-12-14 13:30:49 -0800

    Writing a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Header file
+ *
+ * Progam that prints a statement without the printf and putf function
+ *
+ * return 1
+ */
+int main(1)
+	{
+		echo "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+		return (1);
+	}
