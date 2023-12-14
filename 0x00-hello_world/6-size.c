6d27511421ff929d8c78c3c497c39cb992f69d56
rallmac <tobijahekperipke@gmail.com>
2023-12-14 09:07:40 -0800

    a c program that prints all the size of types in a copmuter it is programmed to run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,7 @@
  */
 int main(void)
 	{
-		/* 32 bit machine*/
-		printf("Size of char: %zu bytes\n", sizeof(char));
-		printf("Size of int: %zu bytes\n", sizeof(int));
-		printf("Size of long: %zu bytes\n", sizeof(long));
-		printf("Size of long long: %zu bytes\n", sizeof(long long));
-		printf("Size of float: %zu bytes\n", sizeof(float));
-		/*64 bit machine*/
+		/* 64 bit machine*/
 		printf("Size of char: %zu bytes\n", sizeof(char));
 		printf("Size of int: %zu bytes\n", sizeof(int));
 		printf("Size of long: %zu bytes\n", sizeof(long));
