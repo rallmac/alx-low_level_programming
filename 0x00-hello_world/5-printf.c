c9a4a7a5a2abd39679f370b5dc5caf5ccd041130
rallmac <tobijahekperipke@gmail.com>
2023-12-14 08:19:25 -0800

    c program that prints a statement using prints function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * his function prints a simple message using printf function.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+	{
+		printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+	}
