908f84c44a511406c6a08eb41209189173a56158
rallmac <tobijahekperipke@gmail.com>
2023-12-15 11:59:35 -0800

    Printing the value stored in the variable n

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ *this program prints the value stored the variable n.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d\n", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Variables, if and if else statement in c
