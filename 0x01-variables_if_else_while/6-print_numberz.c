33a0d863b9d3d6c3df0ee8137d0126c6334671d6
rallmac <tobijahekperipke@gmail.com>
2023-12-16 13:40:57 -0800

    writing a code that prints all the numbers in base 10 sartig from 0 w…

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,30 +1,24 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
  * main - Entry point of the program
  *
- *this program prints all the single numbers in base 10 starting from 0.
+ * this program prints all the single numbers in base 10 starting from 0.
  *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	for (int digit = 0; digit <= 9; digit++)
+	{
+		putchar('0' + digit);
+	}
+
 	putchar('\n');
 
 	return (0);
 }
+
 
 
