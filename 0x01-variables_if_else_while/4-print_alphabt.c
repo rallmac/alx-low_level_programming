aa7735dde70349955c888f13b3ab018ac88f00de
rallmac <tobijahekperipke@gmail.com>
2023-12-16 04:30:23 -0800

    Writing a program that prints the alphabet in lowercase, followed by a new line without e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,18 +5,21 @@
 /**
  * main - Entry point of the program
  *
- *this program prints the alpahabet in lowercase.
+ *this program prints the alpahabet in lowercase without e and q.
  *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	char a;
+	char a = 'a';
 
-	for (a = 'a'; a <= 'z'; a++)
+	while (a <= 'z')
 	{
-		putchar(a);
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		} a++;
 	} putchar('\n');
 	return (0);
 }
