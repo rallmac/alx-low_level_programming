61e621fdc562751d642bd35aa1ea4085a94a313f
rallmac <tobijahekperipke@gmail.com>
2023-12-16 03:58:39 -0800

    Writing a program that prints the alphabet in lowercase, followed by a new line_getting paid off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ *this program prints the alpahabet in lowercase.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	char A;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	} putchar('\n');
+	return (0);
+}
+
