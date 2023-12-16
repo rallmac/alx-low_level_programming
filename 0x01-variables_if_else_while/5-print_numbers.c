2bb55986a325e3b3f0dd6f6666539dba591c5abf
rallmac <tobijahekperipke@gmail.com>
2023-12-16 08:53:59 -0800

    writing a code that prints all the numbers in base 10 sartig from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ *this program prints all the single numbers in base 10 starting from 0.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+
+	return (0);
+}
+
