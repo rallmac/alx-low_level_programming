354b214375897fdeb9c1d7a31bc5ab434afc5a12
rallmac <tobijahekperipke@gmail.com>
2023-12-16 19:07:24 -0800

    writing a program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ *this program prints the alpahabet in lowercase.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		putchar(',');
+		putchar(' ');
+	} putchar('\n');
+	return (0);
+}
+
