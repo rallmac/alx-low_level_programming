fa707c725de3a98b25e5d3dfcf886a8a8286b88c
rallmac <tobijahekperipke@gmail.com>
2023-12-18 23:55:27 -0800

    writing a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * this program prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
