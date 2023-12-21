2b9869daddaf90ccb021487eda39e38c0aac9ef0
rallmac <tobijahekperipke@gmail.com>
2023-12-21 03:18:48 -0800

    writing a function that computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,30 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _abs - Computes the absolute value of an integer
  * @n: The integer for which the absolute value is computed
  *
- * Return: The absolute value of n
+ * Return: The absolute value of number(n) or zero
  */
 int _abs(int n)
 {
-	return ((n < 0) ? -n : n);
-}
+	if (n < 0)
+	{
+	int abs_val;
 
-/**
- * main - Entry point of the program
- *
- * Return: 0 (Success)
- */
-
-int main(void)
-{
-	int number = -42;
-	int absValue = _abs(number);
-
-	printf("The absolute value of %d is: %d\n", number, absValue);
-
-	return (0);
+	abs_val = n * -1;
+		return (abs_val);
+	}
+	return (n);
 }
