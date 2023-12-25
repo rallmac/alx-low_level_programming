8a30e9071bda6f247c9961a49904024a29b948a0
rallmac <tobijahekperipke@gmail.com>
2023-12-25 08:25:36 -0800

    writting a programm that prints FizzBuzz in place of 5 and 3 multiples

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,7 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			print("FizzBuzz");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
