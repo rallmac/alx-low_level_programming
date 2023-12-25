d2eadbc46c0e845e03ad638e7dda2f7fab1092e7
rallmac <tobijahekperipke@gmail.com>
2023-12-25 08:23:50 -0800

    writting a programm that prints FizzBuzz in place of 5 and 3 multiples

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * this programm prints fizz in place of the multiples of 3
+ * and Buzz in place of the multiples of 5
+ * and FizzBuzz in place of a number that is 3 and 5
+ * within the number range of 1 to 100
+ *
+ * Return: Always return 0,
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			print("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
