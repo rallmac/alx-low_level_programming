011271770023ffcb12cad756baf13bddf88f8a19
rallmac <tobijahekperipke@gmail.com>
2024-01-06 03:43:02 -0800

    writing a function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,12 +1,37 @@
 #include "main.h"
 
 /**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
  * *infinite_add - adds characters infinitly
  * @n1: first digit
  * @n2: second digit
  * @r: A character
  * @size_r: A character size
- * rev_string - capital of a string
  * Return: always 0
  */
 
