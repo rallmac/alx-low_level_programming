dd1a54a2146ee4ea346c1b2fd7ad5a7a22188cc4
rallmac <tobijahekperipke@gmail.com>
2024-01-14 09:19:31 -0800

    writting a program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,13 @@
+#include "main.h"
 #include <stdio.h>
+
+/**
+ * main - prints argument count and vector
+ * @argc: number of arguments
+ * @argv: printed vector of argument
+ *
+ * Return: 0 Always
+ */
 
 int main(int argc, char *argv[])
 {
@@ -7,5 +16,5 @@ int main(int argc, char *argv[])
 		printf("%s\n", argv[0]);
 	}
 
-	return 0;
+	return (0);
 }
