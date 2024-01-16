caadfd22053e7e2f09e04fbe84ff7a0de72fe427
rallmac <tobijahekperipke@gmail.com>
2024-01-16 08:55:23 -0800

    writting a function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,49 +1,52 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * is_space - space in the program
- * main - converts string to words
- * @c: space character
- *
- * Return: 1 if error and 0 if success
+ * is_space - checks if a character is a space
+ * @c: character to check
+ * Return: 1 if space, 0 otherwise
  */
-
 int is_space(char c)
 {
 	return (c == ' ' || c == '\t' || c == '\n');
 }
 
-
-	int count_words(char *str)
-	{
-		int in_word, count;
+/**
+ * count_words - counts the number of words in a string
+ * @str: input string
+ * Return: number of words
+ */
+int count_words(char *str)
+{
+	int in_word, count = 0;
 
 	while (*str)
 	{
 		if (is_space(*str))
-		{
-			in_word = 0;
-		}
-		else if (!in_word)
-		{
-			in_word = 1;
-			count++;
-		}
-		str++;
+	{
+		in_word = 0;
+	}
+	else if (!in_word)
+	{
+		in_word = 1;
+		count++;
+	}
+	str++;
 	}
 
 	return (count);
 }
 
 /**
- * strtow - string to word
- *
+ * strtow - splits a string into words
+ * @str: input string
+ * Return: array of words
+ */
 char **strtow(char *str)
 {
 	int num_words, i, word_length, j;
-	char **words, **tab;
+	char **words;
 
 	if (str == NULL || *str == '\0')
 	{
@@ -68,71 +71,41 @@ char **strtow(char *str)
 
 	while (*str)
 	{
-		if (!is_space(*str))
-		{
-			word_length = 0;
+	if (!is_space(*str))
+	{
+		word_length = 0;
 
-			while (*str && !is_space(*str))
-			{
-				word_length++;
-				str++;
-			}
+	while (*str && !is_space(*str))
+	{
+		word_length++;
+		str++;
+	}
 
-			words[i] = malloc((word_length + 1) * sizeof(char));
-		if (words[i] == NULL)
-		{
-
+	words[i] = malloc((word_length + 1) * sizeof(char));
+	if (words[i] == NULL)
+	{
 		for (j = 0; j < i; j++)
 		{
 			free(words[j]);
 		}
 		free(words);
 		return (NULL);
-		}
+	}
 
-		for (j = 0; j < word_length; j++)
-		{
-			words[i][j] = *(str - word_length + j);
-		}
+	for (j = 0; j < word_length; j++)
+	{
+		words[i][j] = *(str - word_length + j);
+	}
 		words[i][word_length] = '\0';
 		i++;
-		}
-		else
-		{
-			str++;
-		}
+	}
+	else
+	{
+		str++;
+	}
 	}
 
 	words[i] = NULL;
 
 	return (words);
-}
-
-int main(void)
-{
-	tab = strtow("      ALX School         #cisfun      ");
-
-	if (tab == NULL)
-	{
-		printf("Failed\n");
-		return (1);
-	}
-
-	print_tab(tab);
-
-	for (i = 0; tab[i] != NULL; i++)
-	{
-		free(tab[i]);
-	}
-	free(tab);
-
-	return (0);
-}
-
-void print_tab(char **tab)
-{
-	for (i = 0; tab[i] != NULL; ++i)
-	{
-		printf("%s\n", tab[i]);
-	}
 }
