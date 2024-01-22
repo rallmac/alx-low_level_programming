510f52f6433dd6962046aa553363925dcb597038
rallmac <tobijahekperipke@gmail.com>
2024-01-22 09:16:58 -0800

    defining a new type struct dog with the elements name, age and owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - the variable to describe
+ * @name:  name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ */
 struct dog
 {
 	char *name;
