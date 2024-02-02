05299000a75c61886722199518ef97d3ed5fd4f7
rallmac2 <root@tobijahdevice>
2024-02-02 05:36:20 +0100

    a 64 bit assembly program that prints "Hello, Holbetron"

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,15 @@
 section .data
-	format db 'Hello, Holberton', 10, 0  ; Define the format string with newline character
+    format db 'Hello, Holberton', 10, 0  ; Define the format string with newline character
 
 section .text
-	global main
-	extern printf
+    global main
+    extern printf
 
 main:
-	mov edi, format        ; Move the address of the format string into the EDI register
-	xor eax, eax           ; Clear the EAX register
-	call printf            ; Call the printf function
-	mov eax, 0             ; Set the return value to 0
-	ret                    ; Return from the main function
+    mov edi, format        ; Move the address of the format string into the EDI register
+    xor eax, eax           ; Clear the EAX register
+    call printf            ; Call the printf function
+    mov eax, 0             ; Set the return value to 0
+    ret                    ; Return from the main function
+
 
