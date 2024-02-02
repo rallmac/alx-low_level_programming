95dba15de3b5c53135b738f7dd8b3ad3d5aac066
rallmac2 <root@tobijahdevice>
2024-02-02 05:19:45 +0100

    advanced task 1 The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("(A tortoise, having preety good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
