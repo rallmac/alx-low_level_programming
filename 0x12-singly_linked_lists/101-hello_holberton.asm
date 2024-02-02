3fe06f37a1cfeb410c3103dae5afa74fc7eafa7e
rallmac2 <root@tobijahdevice>
2024-02-02 05:33:26 +0100

    assembly program

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+	format db 'Hello, Holberton', 10, 0  ; Define the format string with newline character
+
+section .text
+	global main
+	extern printf
+
+main:
+	mov edi, format        ; Move the address of the format string into the EDI register
+	xor eax, eax           ; Clear the EAX register
+	call printf            ; Call the printf function
+	mov eax, 0             ; Set the return value to 0
+	ret                    ; Return from the main function
+
