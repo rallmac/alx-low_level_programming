c7a5077a78df935faa02aa9a9cacbd7631649c85
rallmac2 <root@tobijahdevice>
2024-03-21 19:31:24 +0100

    Get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
+ * @head: Pointer to the head of the list.
+ * @index: Index of the node, starting from 0.
+ *
+ * Return: Pointer to the nth node, or NULL if the node does not exist.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int count = 0;
+
+	current = head;
+	while (current != NULL && count < index)
+	{
+		current = current->next;
+		count++;
+	}
+	return (current);
+}
