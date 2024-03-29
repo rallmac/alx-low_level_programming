49102d2cab14d3400c51ca4e176e216b50416a0a
rallmac2 <root@tobijahdevice>
2024-03-29 10:34:13 +0100

    lottery 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/rallmac/alx-low_level_programming/master/0x18-dynamic_libraries/custompf.so
-export LD_PRELOAD="$PWD/../custompf.so"
+wget -P .. https://raw.githubusercontent.com/rallmac/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
