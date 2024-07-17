e255521a85b138d70fc48eca6258fdc89030e09c
rallmac2 <root@tobijahdevice>
2024-07-17 18:16:30 +0100

    task 7 and 8

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - This algorithm searches for a value in a sorted array
+ * using the Jump search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: The value to search for
+ * Return: The index where value is located, or -1 if not present
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t step = sqrt(size);
+	size_t prev = 0;
+	size_t start;
+	size_t end;
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (array[prev] < value && prev < size)
+	{
+		prev += step;
+	}
+
+	start = prev - step;
+	end = prev < size ? prev : size - 1;
+
+	for (i = start; i <= end; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+		{
+			return ((int)i);
+		}
+	}
+
+	return (-1);
+}
